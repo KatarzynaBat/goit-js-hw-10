293f45b1066128cd679bbc852a10514f37793f2d
KatarzynaBat <117004275+KatarzynaBat@users.noreply.github.com>
2023-12-17 12:23:57 +0000

    Deploying to gh-pages from @ KatarzynaBat/goit-js-hw-10@6a267571d6e9a4b3838d3f0bcdf5d007b3d85184 🚀

--- index.602216e5.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds").then((t=>(console.log(t),t.json()))).then((t=>{})).catch((t=>{}));
-//# sourceMappingURL=index.602216e5.js.map
--- index.8ae652a1.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds").then((function(t){return console.log(t),t.json()})).then((function(t){})).catch((function(t){}));
-//# sourceMappingURL=index.8ae652a1.js.map
